187a8673369e765746554761b6120ef6722f5128
Charlotte <devy1u>
2016-05-21 20:00:51 +0200

    Fonctions supplementaires

---
+++ utils.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "utils.h"
+
+long taille_double(double d) {
+    double res = 1 + log10(d); 
+    return (long) res;
+}
+
+long taille_long(long l) {
+    double res = 1 + log10(l); 
+    return (long) res;
+}
+
+void vider_buffer() {
+    int c;
+    while ((c = getchar ()) != '\n' && c != EOF);
+}
---
+++ utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+/*
+Definit des fonctions utiles au projet
+*/
+
+long taille_double(double d);
+
+long taille_long(long l);
+
+void vider_buffer();
+
+#endif
