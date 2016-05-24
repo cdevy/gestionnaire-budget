c206ffe4e44dd817bdaef3045d87541f0ddbc41d
Basselin Justine <juju@localhost.localdomain>
2016-05-24 08:37:54 +0200

    test de lecture

--- lecture.c
+++ lecture.c
@@ -12,7 +12,8 @@ int main(int argc, char **argv)
 	//printf("veuillez entrer le chemin de votre relever de compte \n");
 	//scanf("%s", &nomDuFichier); 
 	
-	char* contenu;
+	char contenu[30];
+	char contenu2[30];
 	//On appelle la fonction d'ouverture de fichier fopen qui nous renvoie un pointeur sur le fichier.	
     FILE* fichier = NULL;
     fichier = fopen("jc", "r");
@@ -21,14 +22,15 @@ int main(int argc, char **argv)
 	//On vérifie si le fichier existe en testant la valeur du pointeur qu'on a reçu. 
 	//Si le pointeur est différent de NULL,
 	//alors on peut lire dans le fichier
-    if (fichier != NULL)
-    {
+    if (fichier != NULL){
+		fscanf(fichier, "%s,%s", contenu, contenu2);
+		while (strcmp(contenu,"--fin--")!=0){
+			printf("Qu'est ce qu'on veut ?? %s \n", contenu);
+			printf("Qu'est ce qu'on veut ?? %s \n", contenu2);
+			fscanf(fichier, "%s,%s", contenu, contenu2);
         
-		fscanf(fichier, "%s", contenu);
-		printf("Qu'est ce qu'on veut ?? %s \n", contenu);
-		fscanf(fichier, "%s", contenu);
-		printf("Qu'est ce qu'on veut pas ?? %s \n", contenu);
-        fclose(fichier);
+		}
+		fclose(fichier);
     }
     
     //Si le pointeur vaut NULL, c'est que l'ouverture du fichier n'a pas fonctionné
